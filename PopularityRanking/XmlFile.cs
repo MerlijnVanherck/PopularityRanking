b5af8aebea9feb907bef599d4526a9889a4ed1a1
Merlijn Vanherck <merlijnvanherck@gmail.com>
2021-01-30 16:18:13 +0100

    xml importing and exporting

---
+++ PopularityRanking/XmlFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PopularityRanking
+{
+    public static class XmlFile
+    {
+        public static T Import<T>(string path)
+        {
+            using StreamReader reader = new StreamReader(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            return (T)serializer.Deserialize(reader);
+        }
+
+        public static void Export<T>(string path, T item)
+        {
+            using StreamWriter writer = new StreamWriter(path);
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(writer, item);
+        }
+    }
+}
